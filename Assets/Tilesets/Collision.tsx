d609353d2610704d720738a5728d710d3a17a725
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-04 00:29:42 +0200

    Add basic outdoor and indoor maps

---
+++ Assets/Tilesets/Collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../../../CardsOld/Maps/Tilesets/red.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="0" x="-0.0888889" y="-0.0666667" width="16.1333" height="16.1333"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Assets/Tilesets/Houses/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Houses - 1" tilewidth="16" tileheight="16" tilecount="1080" columns="20">
+ <image source="1.png" width="320" height="864"/>
+</tileset>
