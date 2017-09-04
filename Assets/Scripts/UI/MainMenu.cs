73f94167d25f5f2a326e3b6aa32f075c97e670ac
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-04 23:34:00 +0200

    Add main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject loadPanel;
+    public GameObject optionsPanel;
+    public GameObject firstSelectedMain;
+    public Slider optionsSfx;
+    public Slider optionsMusic;
+    private bool optionsChanged = false;
+
+	#region Main
+
+    public void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(firstSelectedMain);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void Load()
+    {
+        mainPanel.SetActive(false);
+        loadPanel.SetActive(true);
+    }
+
+    public void Options()
+    {
+        OptionsLoad();
+
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    #region Options
+
+    public void OptionsLoad()
+    {
+        optionsSfx.value = PlayerPrefs.GetInt("Sfx", 8);
+        optionsMusic.value = PlayerPrefs.GetInt("Music", 8);
+
+        optionsChanged = false;
+    }
+
+    public void OptionsSave()
+    {
+        PlayerPrefs.SetInt("Sfx", (int)optionsSfx.value);
+        PlayerPrefs.SetInt("Music", (int)optionsMusic.value);
+
+        optionsChanged = false;
+    }
+
+    public void OptionsBack()
+    {
+        if (optionsChanged)
+        {
+            OptionsSave();
+        }
+
+        optionsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void OptionsSfx()
+    {
+        Debug.Log("Update SFX volume");
+        optionsChanged = true;
+    }
+
+    public void OptionsMusic()
+    {
+        Debug.Log("Update music volume");
+        optionsChanged = true;
+    }
+
+    #endregion
+
+    #region Load
+
+    public void LoadBack()
+    {
+        loadPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/SaveSlot.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SaveSlot : MonoBehaviour
+{
+    public int slot;
+    public GameObject saveButton = null;
+    public GameObject loadButton = null;
+    public GameObject deleteButton = null;
+
+    void OnEnable()
+    {
+        UpdateStatus();
+    }
+
+    void Start()
+    {
+        if (saveButton != null)
+        {
+            saveButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SaveManager.Save(slot);
+                UpdateStatus();
+            });
+        }
+
+        if (loadButton != null)
+        {
+            loadButton.GetComponent<Button>().onClick.AddListener(() =>
+			{
+				if (SceneManager.GetActiveScene().name != "World")
+				{
+					SaveManager.mustLoad = slot;
+					SceneManager.LoadScene("World");
+				}
+				else
+				{
+					SaveManager.Load(slot);
+					UpdateStatus();
+				}
+            });
+        }
+
+        if (deleteButton != null)
+        {
+            deleteButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SaveManager.Delete(slot);
+                UpdateStatus();
+            });
+        }
+    }
+
+    public void UpdateStatus()
+    {
+        bool enabled = SaveManager.HasSave(slot);
+
+        if (loadButton != null)
+        {
+            loadButton.GetComponent<Button>().interactable = enabled;
+        }
+        if (deleteButton)
+        {
+            deleteButton.GetComponent<Button>().interactable = enabled;
+        }
+    }
+}
