2e9b9a8d3fa8cf536db824937a714f8e098695f9
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-04 23:07:06 +0200

    Add screen fade animation

---
+++ Assets/Scripts/UI/ScreenFader.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ScreenFader : MonoBehaviour
+{
+    private Animator animator;
+    private bool isFading = false;
+
+	void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public IEnumerator FadeToClear()
+    {
+        isFading = true;
+        animator.SetTrigger("Fade In");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeToBlack()
+    {
+        isFading = true;
+        animator.SetTrigger("Fade Out");
+
+        while (isFading)
+        {
+            yield return null;
+        }
+    }
+
+    public void AnimationComplete()
+    {
+        isFading = false;
+	}
+}
