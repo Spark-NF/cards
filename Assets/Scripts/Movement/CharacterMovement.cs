4e3b4283ff3b1041fd1291608bf36820c0557203
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-04 22:25:26 +0200

    Add basic character with movement

---
+++ Assets/Scripts/Movement/CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public abstract class CharacterMovement : MonoBehaviour
+{
+	protected Rigidbody2D rbody;
+	protected Animator anim;
+	protected bool frozen = false;
+	public float speed = 1.0f;
+
+	private void Start()
+	{
+		rbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+	}
+
+	public void LookAt(Vector2 movementVector)
+	{
+		anim.SetFloat("input_x", movementVector.x);
+		anim.SetFloat("input_y", movementVector.y);
+	}
+
+	protected void Move(Vector2 movementVector)
+	{
+		if (frozen)
+			return;
+
+		bool hasVector = movementVector != Vector2.zero;
+		anim.SetBool("is_walking", hasVector);
+		if (hasVector)
+		{
+			LookAt(movementVector);
+		}
+
+		rbody.MovePosition(rbody.position + movementVector * Time.deltaTime * speed);
+	}
+
+	public void SetFrozen(bool frz)
+	{
+		frozen = frz;
+
+		if (frz)
+		{
+			anim.SetBool("is_walking", false);
+		}
+	}
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : CharacterMovement
+{
+	public GameObject front;
+
+	private void Update()
+	{
+		// Move player according to input (keyboard and mobile)
+		Vector2 movementVector = new Vector2(
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical")
+		);
+		Move(movementVector);
+
+		// Update eyes
+		/*if (movementVector != Vector2.zero && !frozen)
+		{
+			int angle;
+			if (Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y))
+			{
+				angle = movementVector.x > 0 ? 90 : -90;
+			}
+			else
+			{
+				angle = movementVector.y > 0 ? 180 : 0;
+			}
+
+			front.transform.rotation = Quaternion.Euler(0, 0, angle);
+		}*/
+	}
+}
