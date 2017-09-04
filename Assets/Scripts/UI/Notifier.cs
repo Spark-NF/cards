abdf7e2ab530d1b1379b485ff23f18540a39afc4
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-05 00:48:46 +0200

    Add notifier

---
+++ Assets/Scripts/UI/Notifier.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Notifier : MonoBehaviour
+{
+	public GameObject canvas;
+	public Text textBox;
+	private Animator anim;
+
+	void Start()
+	{
+		canvas.SetActive(false);
+		anim = canvas.GetComponent<Animator>();
+	}
+
+	public IEnumerator Notify(string txt, float duration = 3f)
+	{
+		if (txt == null)
+		{
+			Clear();
+			yield break;
+		}
+
+		textBox.text = txt.Replace("\\n", "\n");
+		canvas.SetActive(true);
+		anim.Play("Slide In");
+
+		yield return new WaitForSeconds(duration);
+
+		anim.Play("Slide Out");
+	}
+
+	public void Clear()
+	{
+		canvas.SetActive(false);
+	}
+    
+    public void Close()
+    {
+        anim.Play("Slide Out");
+    }
+}
