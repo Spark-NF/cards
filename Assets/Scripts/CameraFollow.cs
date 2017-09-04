9b8993c3d14ebdc8f0b137460d3477e8394aed84
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-04 22:41:21 +0200

    Add camera following the player character

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+	public float lerp = 0.1f;
+	private float initZ;
+
+	void Start()
+	{
+		initZ = transform.position.z;
+	}
+
+	void Update()
+	{
+		if (!target)
+			return;
+
+		Vector3 targetCamera = Vector3.Lerp(transform.position, target.position, lerp);
+		targetCamera.z = initZ;
+
+		transform.position = targetCamera;
+	}
+}
