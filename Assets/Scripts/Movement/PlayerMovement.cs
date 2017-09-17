318da8944e1ab4df57e4fe5e9b06c7b98c5f79d3
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-18 01:38:58 +0200

    Properly move character eyes when moving

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -14,19 +14,13 @@ public class PlayerMovement : CharacterMovement
 		Move(movementVector);
 
 		// Update eyes
-		/*if (movementVector != Vector2.zero && !Frozen)
+		if (movementVector != Vector2.zero && !Frozen)
 		{
-			int angle;
-			if (Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y))
-			{
-				angle = movementVector.x > 0 ? 90 : -90;
-			}
-			else
-			{
-				angle = movementVector.y > 0 ? 180 : 0;
-			}
+			int angle = Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y)
+				? movementVector.x > 0 ? 90 : -90
+				: movementVector.y > 0 ? 180 : 0;
 
 			Front.transform.rotation = Quaternion.Euler(0, 0, angle);
-		}*/
+		}
 	}
 }
