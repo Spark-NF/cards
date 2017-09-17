e74e316da58b1a5dda5adc88d4885ef00fc9a85d
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-18 01:40:39 +0200

    Allow to interact and talk with NPCs

---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,44 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+	private Collider2D _currentCollider = null;
+	private Flowchart _currentInteractible = null;
+
+	void Update()
+	{
+		if (CnControls.CnInputManager.GetButtonDown("Action"))
+		{
+			if (_currentInteractible != null)
+			{
+				_currentInteractible.ExecuteBlock("Init");
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		var obj = other.gameObject;
+		if (obj == null)
+			return;
+
+		var flowchart = obj.GetComponent<Flowchart>();
+		if (flowchart == null)
+			return;
+
+		_currentCollider = other;
+		_currentInteractible = flowchart;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other == _currentCollider)
+		{
+			_currentCollider = null;
+			_currentInteractible = null;
+		}
+	}
+}
