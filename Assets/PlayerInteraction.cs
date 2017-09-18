74675aac09c186f5f5b3f8e0c1a7216cbbc3d868
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-18 15:03:43 +0200

    Allow multiple interactibles at the same time in front of the player

--- Assets/PlayerInteraction.cs
+++ Assets/PlayerInteraction.cs
@@ -1,42 +1,50 @@
-﻿using Fungus;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fungus;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-	private Collider2D _currentCollider = null;
-	private Flowchart _currentInteractible = null;
+	private List<GameObject> _interactibles = new List<GameObject>();
 
 	public void Update()
 	{
+		// We always need something to interact with
+		if (_interactibles.Count == 0)
+			return;
+
+		// Interact with the object in view when triggering the "Action" control
 		if (CnControls.CnInputManager.GetButtonDown("Action"))
 		{
-			if (_currentInteractible != null)
+			// If there are multiple objects in front of us, we sort them by distance to the player
+			if (_interactibles.Count > 1)
 			{
-				_currentInteractible.SendFungusMessage("Start");
+				_interactibles = _interactibles.OrderBy(
+					x => Vector2.Distance(transform.parent.position, x.transform.position)
+				).ToList();
+			}
+
+			// We try each object in front of us until we find an interactible one
+			foreach (var obj in _interactibles)
+			{
+				// Interact using a Fungus Flowchart
+				var flowchart = obj.GetComponent<Flowchart>();
+				if (flowchart != null)
+				{
+					flowchart.SendFungusMessage("Start");
+					return;
+				}
 			}
 		}
 	}
 
 	public void OnTriggerEnter2D(Collider2D other)
 	{
-		var obj = other.gameObject;
-		if (obj == null)
-			return;
-
-		var flowchart = obj.GetComponent<Flowchart>();
-		if (flowchart == null)
-			return;
-
-		_currentCollider = other;
-		_currentInteractible = flowchart;
+		_interactibles.Add(other.gameObject);
 	}
 
 	public void OnTriggerExit2D(Collider2D other)
 	{
-		if (other == _currentCollider)
-		{
-			_currentCollider = null;
-			_currentInteractible = null;
-		}
+		_interactibles.Remove(other.gameObject);
 	}
 }
