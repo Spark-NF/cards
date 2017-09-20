c09e53c25f1b567c4b985a21a0cb0f1a48d4d484
lelimacon <lelimacon@users.noreply.github.com>
2017-09-20 21:38:29 +0200

    Inventory menu

---
+++ Assets/Scripts/UI/InventoryMenu.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryMenu : MonoBehaviour
+{
+	public GameObject MainCanvas;
+	public GameObject BagPanel;
+	public GameObject CardsPanel;
+	public GameObject ExitInventoryButton;
+	public bool StopTime = true;
+
+	void Start()
+	{
+		ShowBag();
+	}
+
+	public void Show()
+	{
+		// Activate the panel
+		MainCanvas.SetActive(true);
+
+		// Select first menu element
+		EventSystem es = EventSystem.current;
+		es.SetSelectedGameObject(ExitInventoryButton);
+
+		// Stop time
+		if (StopTime)
+		{
+			Time.timeScale = 0f;
+		}
+	}
+
+	public void ShowBag()
+	{
+		// Switch the panels
+		BagPanel.SetActive(true);
+		CardsPanel.SetActive(false);
+	}
+
+	public void ShowCards()
+	{
+		// Switch the panels
+		BagPanel.SetActive(false);
+		CardsPanel.SetActive(true);
+	}
+
+	public void Hide()
+	{
+		// Deactivate the panel
+		MainCanvas.SetActive(false);
+
+		// Resume time
+		if (StopTime)
+		{
+			Time.timeScale = 1f;
+		}
+	}
+	
+	void Update()
+	{
+	}
+}
--- Assets/Scripts/UI/InventoryPanel.cs
+++ Assets/Scripts/UI/InventoryPanel.cs
@@ -1,12 +1,8 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public class InventoryPanel : MonoBehaviour
 {
-	public Text MoneyCounter;
-
 	private void Update ()
 	{
-		MoneyCounter.text = Game.Current.Player.Inventory.Money.ToString();
 	}
 }
