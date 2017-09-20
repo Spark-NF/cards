2e5c5af3e9c39a8e27f3485d69b6f716d82ea071
Nicolas Faure <spark_ps@hotmail.fr>
2017-09-20 02:12:20 +0200

    Add outline to currently active interactible

--- Assets/Scripts/Interactions/PlayerInteraction.cs
+++ Assets/Scripts/Interactions/PlayerInteraction.cs
@@ -9,55 +9,46 @@ public class PlayerInteraction : MonoBehaviour
 	public SayDialog SmallSayDialog;
 	public RectTransform SmallSayDialogTransform;
 	public SayDialog FullSizeSayDialog;
+	public float OutlineSize = 0.05f;
+	public Color OutlineColor = Color.red;
 
 	private List<GameObject> _interactibles = new List<GameObject>();
+	private GameObject _activeInteractible;
+	private Shader _oldShader;
 
 	public void Update()
 	{
 		// We always need something to interact with
-		if (_interactibles.Count == 0)
+		if (_activeInteractible == null)
 			return;
 
 		// Interact with the object in view when triggering the "Action" control
 		if (CnControls.CnInputManager.GetButtonDown("Action"))
 		{
-			// If there are multiple objects in front of us, we sort them by distance to the player
-			if (_interactibles.Count > 1)
+			// Interact using a Fungus Flowchart
+			var flowchart = _activeInteractible.GetComponent<Flowchart>();
+			if (flowchart != null)
 			{
-				_interactibles = _interactibles.OrderBy(
-					x => Vector3.Distance(transform.parent.position, x.transform.position)
-				).ToList();
-			}
+				bool basic = Random.value > 0.5f;
 
-			// We try each object in front of us until we find an interactible one
-			foreach (var obj in _interactibles)
-			{
-				// Interact using a Fungus Flowchart
-				var flowchart = obj.GetComponent<Flowchart>();
-				if (flowchart != null)
+				if (basic)
 				{
-					bool basic = Random.value > 0.5f;
+					SayDialog.ActiveSayDialog = SmallSayDialog;
 
-					if (basic)
-					{
-						SayDialog.ActiveSayDialog = SmallSayDialog;
-
-						// Show small dialog above interactible
-						SmallSayDialogTransform.position = obj.transform.position + new Vector3(0, 2f, 0);
-						SmallSayDialogTransform.pivot = new Vector2(0.5f, 0.5f);
-					}
-					else
-					{
-						SayDialog.ActiveSayDialog = FullSizeSayDialog;
-
-						// Freeze player
-						MovementController.IsFrozen = true;
-						BlockSignals.OnBlockEnd += UnfreezePlayerWhenFlowchartEnds;
-					}
-
-					flowchart.SendFungusMessage("Start");
-					return;
+					// Show small dialog above interactible
+					SmallSayDialogTransform.position = _activeInteractible.transform.position + new Vector3(0, 2f, 0);
+					SmallSayDialogTransform.pivot = new Vector2(0.5f, 0.5f);
 				}
+				else
+				{
+					SayDialog.ActiveSayDialog = FullSizeSayDialog;
+
+					// Freeze player
+					MovementController.IsFrozen = true;
+					BlockSignals.OnBlockEnd += UnfreezePlayerWhenFlowchartEnds;
+				}
+
+				flowchart.SendFungusMessage("Start");
 			}
 		}
 	}
@@ -73,13 +64,54 @@ public class PlayerInteraction : MonoBehaviour
 		}
 	}
 
+	private void UpdateInteractible()
+	{
+		if (_activeInteractible != null)
+			_activeInteractible.GetComponent<Renderer>().material.shader = _oldShader;
+
+		_activeInteractible = RefreshInteractible();
+
+		if (_activeInteractible != null)
+		{
+			var material = _activeInteractible.GetComponent<Renderer>().material;
+			_oldShader = material.shader;
+			material.shader = Shader.Find("Custom/Outline");
+			material.SetFloat("_Outline", OutlineSize);
+			material.SetColor("_OutlineColor", OutlineColor);
+		}
+	}
+
+	private GameObject RefreshInteractible()
+	{
+		// If there are multiple objects in front of us, we sort them by distance to the player
+		if (_interactibles.Count > 1)
+		{
+			_interactibles = _interactibles.OrderBy(
+				x => Vector3.Distance(transform.parent.position, x.transform.position)
+			).ToList();
+		}
+
+		// We try each object in front of us until we find an interactible one
+		foreach (var obj in _interactibles)
+		{
+			// Interact using a Fungus Flowchart
+			var flowchart = obj.GetComponent<Flowchart>();
+			if (flowchart != null)
+				return obj;
+		}
+
+		return null;
+	}
+
 	public void OnTriggerEnter(Collider other)
 	{
 		_interactibles.Add(other.gameObject);
+		UpdateInteractible();
 	}
 
 	public void OnTriggerExit(Collider other)
 	{
 		_interactibles.Remove(other.gameObject);
+		UpdateInteractible();
 	}
 }
